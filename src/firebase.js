ba118fc44932d23df2e339535dfa20ddc6863d16
DHAIRYA GULATI <dgulati352@gmail.com>
2026-05-18 00:41:18 +0530

    Configure correct databaseURL in firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -9,7 +9,8 @@ const firebaseConfig =  {
   storageBucket: "lavasa-52e19.firebasestorage.app",
   messagingSenderId: "487517193462",
   appId: "1:487517193462:web:7caddd9b4e0cec5a1658d7",
-  measurementId: "G-PRY4H1CGDV"
+  measurementId: "G-PRY4H1CGDV",
+  databaseURL: "https://lavasa-52e19-default-rtdb.firebaseio.com"
 };
 
 const app = initializeApp(firebaseConfig);
